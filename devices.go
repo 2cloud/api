db5b62ee9bb9a9ec9823fa78cfa4a8a7e3a5e2b9
Paddy Foran <foran.paddy@gmail.com>
2012-11-19 12:28:06 -0500

    Stubbed out device and link endpoints.

---
+++ devices.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"get.2cloud.org/twocloud"
+	"net/http"
+)
+
+func getDevices(w http.ResponseWriter, r *twocloud.RequestBundle) {
+}
+
+func newDevice(w http.ResponseWriter, r *twocloud.RequestBundle) {
+}
+
+func getDevice(w http.ResponseWriter, r *twocloud.RequestBundle) {
+}
+
+func updateDevice(w http.ResponseWriter, r *twocloud.RequestBundle) {
+}
+
+func deleteDevice(w http.ResponseWriter, r *twocloud.RequestBundle) {
+}
---
+++ links.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"get.2cloud.org/twocloud"
+	"net/http"
+)
+
+func getLinksByUser(w http.ResponseWriter, r *twocloud.RequestBundle) {
+}
+
+func getLinksByDevice(w http.ResponseWriter, r *twocloud.RequestBundle) {
+}
+
+func sendLink(w http.ResponseWriter, r *twocloud.RequestBundle) {
+}
+
+func getLink(w http.ResponseWriter, r *twocloud.RequestBundle) {
+}
+
+func updateLink(w http.ResponseWriter, r *twocloud.RequestBundle) {
+}
+
+func deleteLink(w http.ResponseWriter, r *twocloud.RequestBundle) {
+}
