6e91b72a4c034818774b2dd8ec6d926705720d22
Paddy Foran <foran.paddy@gmail.com>
2012-11-19 14:03:42 -0500

    Implemented adding a new device. Fixes #15.

--- devices.go
+++ devices.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"get.2cloud.org/twocloud"
+	"io/ioutil"
 	"net/http"
 	"secondbit.org/ruid"
 	"strings"
@@ -80,6 +82,55 @@ func getDevice(w http.ResponseWriter, r *twocloud.RequestBundle) {
 }
 
 func newDevice(w http.ResponseWriter, r *twocloud.RequestBundle) {
+	username := r.Request.URL.Query().Get(":username")
+	user := r.AuthUser
+	if strings.ToLower(username) != strings.ToLower(r.AuthUser.Username) {
+		if !r.AuthUser.IsAdmin {
+			Respond(w, r, http.StatusUnauthorized, "You don't have access to that user's devices.", []interface{}{})
+			return
+		}
+		id, err := r.GetUserID(username)
+		if err != nil {
+			r.Log.Error(err.Error())
+			Respond(w, r, http.StatusInternalServerError, "Internal server error", []interface{}{})
+			return
+		}
+		user, err = r.GetUser(id)
+		if err != nil {
+			r.Log.Error(err.Error())
+			Respond(w, r, http.StatusInternalServerError, "Internal server error", []interface{}{})
+			return
+		}
+	}
+	var req twocloud.Device
+	body, err := ioutil.ReadAll(r.Request.Body)
+	if err != nil {
+		r.Log.Error(err.Error())
+		Respond(w, r, http.StatusInternalServerError, "Internal server error.", []interface{}{})
+		return
+	}
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		r.Log.Error(err.Error())
+		Respond(w, r, http.StatusBadRequest, "Error decoding request.", []interface{}{})
+		return
+	}
+	if req.Name == "" {
+		Respond(w, r, http.StatusBadRequest, "Name must be specified.", []interface{}{})
+		return
+	}
+	req.ClientType = strings.ToLower(req.ClientType)
+	if !req.ValidClientType() {
+		Respond(w, r, http.StatusBadRequest, "Invalid client type.", []interface{}{})
+		return
+	}
+	device, err := r.AddDevice(req.Name, req.ClientType, r.Request.RemoteAddr, req.Pushers.GCM.Key, user)
+	if err != nil {
+		Respond(w, r, http.StatusInternalServerError, "Internal server error.", []interface{}{})
+		return
+	}
+	Respond(w, r, http.StatusCreated, "Successfully created a device", []interface{}{device})
+	return
 }
 
 func updateDevice(w http.ResponseWriter, r *twocloud.RequestBundle) {
