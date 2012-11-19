e8555fb381fadd4329c3e1bde2d090774d9c309d
Paddy Foran <foran.paddy@gmail.com>
2012-11-19 13:11:18 -0500

    Implemented retrieving a device. Fixes #16.

--- devices.go
+++ devices.go
@@ -3,6 +3,7 @@ package main
 import (
 	"get.2cloud.org/twocloud"
 	"net/http"
+	"secondbit.org/ruid"
 	"strings"
 )
 
@@ -37,7 +38,44 @@ func getDevices(w http.ResponseWriter, r *twocloud.RequestBundle) {
 	return
 }
 
-func newDevice(w http.ResponseWriter, r *twocloud.RequestBundle) {
+func newDevice(w http.ResponseWriter, r *twocloud.RequestBundle) {	username := r.Request.URL.Query().Get(":username")
+	user := r.AuthUser
+	if strings.ToLower(username) != strings.ToLower(r.AuthUser.Username) {
+		if !r.AuthUser.IsAdmin {
+			Respond(w, r, http.StatusUnauthorized, "You don't have access to that user's devices.", []interface{}{})
+			return
+		}
+		id, err := r.GetUserID(username)
+		if err != nil {
+			r.Log.Error(err.Error())
+			Respond(w, r, http.StatusInternalServerError, "Internal server error", []interface{}{})
+			return
+		}
+		user, err = r.GetUser(id)
+		if err != nil {
+			r.Log.Error(err.Error())
+			Respond(w, r, http.StatusInternalServerError, "Internal server error", []interface{}{})
+			return
+		}
+	}
+	id, err := ruid.RUIDFromString(r.Request.URL.Query().Get(":device"))
+	if err != nil {
+		r.Log.Error(err.Error())
+		Respond(w, r, http.StatusInternalServerError, "Internal server error", []interface{}{})
+		return
+	}
+	device, err := r.GetDevice(id)
+	if err != nil {
+		r.Log.Error(err.Error())
+		Respond(w, r, http.StatusInternalServerError, "Internal server error", []interface{}{})
+		return
+	}
+	if device.UserID != user.ID {
+		Respond(w, r, http.StatusBadRequest, "That device ID does not belong to that user.", []interface{}{})
+		return
+	}
+	Respond(w, r, http.StatusOK, "Successfully retrieved device information", []interface{}{device})
+	return
 }
 
 func getDevice(w http.ResponseWriter, r *twocloud.RequestBundle) {
