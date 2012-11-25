46072a70c3a92d77c4e13e30473fd6848e4214e8
Paddy Foran <foran.paddy@gmail.com>
2012-11-25 15:13:08 -0500

    Implemented getting details about a notification. Fixes #30.

--- notifications.go
+++ notifications.go
@@ -97,6 +97,54 @@ func getNotifications(w http.ResponseWriter, r *twocloud.RequestBundle) {
 }
 
 func getNotification(w http.ResponseWriter, r *twocloud.RequestBundle) {
+	username := r.Request.URL.Query().Get(":username")
+	user := r.AuthUser
+	if strings.ToLower(username) != strings.ToLower(r.AuthUser.Username) {
+		if !r.AuthUser.IsAdmin {
+			Respond(w, r, http.StatusUnauthorized, "You don't have access to that user's links.", []interface{}{})
+			return
+		}
+		id, err := r.GetUserID(username)
+		if err != nil {
+			r.Log.Error(err.Error())
+			Respond(w, r, http.StatusInternalServerError, "Internal server error", []interface{}{})
+			return
+		}
+		user, err = r.GetUser(id)
+		if err != nil {
+			r.Log.Error(err.Error())
+			Respond(w, r, http.StatusInternalServerError, "Internal server error", []interface{}{})
+			return
+		}
+	}
+	deviceID, err := ruid.RUIDFromString(r.Request.URL.Query().Get(":device"))
+	if err != nil {
+		r.Log.Error(err.Error())
+		Respond(w, r, http.StatusBadRequest, "Invalid device ID", []interface{}{})
+		return
+	}
+	device, err := r.GetDevice(deviceID)
+	if err != nil {
+		r.Log.Error(err.Error())
+		Respond(w, r, http.StatusInternalServerError, "Internal server error", []interface{}{})
+		return
+	}
+	if device.UserID != user.ID {
+		Respond(w, r, http.StatusBadRequest, "That device ID does not belong to that user.", []interface{}{})
+		return
+	}
+	notificationID, err := ruid.RUIDFromString(r.Request.URL.Query().Get(":notification"))
+	if err != nil {
+		Respond(w, r, http.StatusBadRequest, "Invalid notification ID", []interface{}{})
+		return
+	}
+	notification, err := r.GetNotification(notificationID)
+	if err != nil {
+		Respond(w, r, http.StatusInternalServerError, "Internal server error", []interface{}{})
+		return
+	}
+	Respond(w, r, http.StatusOK, "Successfully retrieved notification information", []interface{}{notification})
+	return
 }
 
 func sendNotification(w http.ResponseWriter, r *twocloud.RequestBundle) {
