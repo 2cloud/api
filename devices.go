1bab965136ef8750495d777f515102f92c719bf2
Paddy Foran <foran.paddy@gmail.com>
2012-11-29 12:55:04 -0500

    Implemented deleting a device. Fixes #18.

--- devices.go
+++ devices.go
@@ -201,6 +201,50 @@ func updateDevice(w http.ResponseWriter, r *twocloud.RequestBundle) {
 }
 
 func deleteDevice(w http.ResponseWriter, r *twocloud.RequestBundle) {
+	username := r.Request.URL.Query().Get(":username")
+	user := r.AuthUser
+	if strings.ToLower(username) != strings.ToLower(r.AuthUser.Username) {
+		if !r.AuthUser.IsAdmin {
+			Respond(w, r, http.StatusUnauthorized, "You don't have access to that user's devices.", []interface{}{})
+			return
+		}
+		id, err := r.GetUserID(username)
+		if err != nil {
+			r.Log.Error(err.Error())
+			Respond(w, r, http.StatusInternalServerError, "Internal server error", []interface{}{})
+			return
+		}
+		user, err = r.GetUser(id)
+		if err != nil {
+			r.Log.Error(err.Error())
+			Respond(w, r, http.StatusInternalServerError, "Internal server error", []interface{}{})
+			return
+		}
+	}
+	id, err := ruid.RUIDFromString(r.Request.URL.Query().Get(":device"))
+	if err != nil {
+		r.Log.Error(err.Error())
+		Respond(w, r, http.StatusInternalServerError, "Internal server error", []interface{}{})
+		return
+	}
+	device, err := r.GetDevice(id)
+	if err != nil {
+		r.Log.Error(err.Error())
+		Respond(w, r, http.StatusInternalServerError, "Internal server error", []interface{}{})
+		return
+	}
+	if device.UserID != user.ID {
+		Respond(w, r, http.StatusBadRequest, "That device ID does not belong to that user.", []interface{}{})
+		return
+	}
+	err = r.DeleteDevice(device)
+	if err != nil {
+		r.Log.Error(err.Error())
+		Respond(w, r, http.StatusInternalServerError, "Internal server error", []interface{}{})
+		return
+	}
+	Respond(w, r, http.StatusOK, "Successfully deleted the device", []interface{}{device})
+	return
 }
 
 func auditDevice(w http.ResponseWriter, r *twocloud.RequestBundle) {
