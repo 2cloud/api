db2bc8c588fdbc150bc20749f863dbbc492448ad
Paddy Foran <foran.paddy@gmail.com>
2012-11-29 16:41:23 -0500

    Implemented canceling subscriptions. Fixes #42.

--- subscriptions.go
+++ subscriptions.go
@@ -184,6 +184,35 @@ func updateSubscription(w http.ResponseWriter, r *twocloud.RequestBundle) {
 }
 
 func cancelSubscription(w http.ResponseWriter, r *twocloud.RequestBundle) {
+	username := r.Request.URL.Query().Get(":username")
+	user := r.AuthUser
+	if strings.ToLower(username) != strings.ToLower(r.AuthUser.Username) {
+		if !r.AuthUser.IsAdmin {
+			Respond(w, r, http.StatusUnauthorized, "You don't have access to that user's subscription.", []interface{}{})
+			return
+		}
+		id, err := r.GetUserID(username)
+		if err != nil {
+			r.Log.Error(err.Error())
+			Respond(w, r, http.StatusInternalServerError, "Internal server error", []interface{}{})
+			return
+		}
+		user, err = r.GetUser(id)
+		if err != nil {
+			r.Log.Error(err.Error())
+			Respond(w, r, http.StatusInternalServerError, "Internal server error", []interface{}{})
+			return
+		}
+	}
+	sub := *user.Subscription
+	err := r.CancelSubscription(user)
+	if err != nil {
+		Respond(w, r, http.StatusInternalServerError, "Internal server error", []interface{}{})
+		return
+	}
+	Respond(w, r, http.StatusOK, "Successfully canceled the subscription", []interface{}{sub})
+	return
+
 }
 
 func auditSubscription(w http.ResponseWriter, r *twocloud.RequestBundle) {
